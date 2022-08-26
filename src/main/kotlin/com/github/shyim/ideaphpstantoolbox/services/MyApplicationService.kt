65ddb0c03be92b0867e6e7c08a0e9c03a70f87d4
GitHub Action <action@github.com>
2022-08-26 09:06:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/shyim/ideaphpstantoolbox/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shyim/ideaphpstantoolbox/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shyim.ideaphpstantoolbox.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shyim.ideaphpstantoolbox.MyBundle
 
 class MyApplicationService {
 
